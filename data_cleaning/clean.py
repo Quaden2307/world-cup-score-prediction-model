281756269c842bbc94445bf73e4b71e3385e55bc
Caden <cadensun07@gmail.com>
2026-06-08 21:33:02 -0700

    cleaning - incomplete

--- data_cleaning/clean.py
+++ data_cleaning/clean.py
@@ -3,6 +3,8 @@ import pandas as pd
 df = pd.read_csv("data/matches.csv")
 df = df[df['tournament_name'].str.contains("Women's", na=False) == False]
 df["total_goals"] = df["home_team_score"] + df["away_team_score"]
-df["is_knockout_stage"]
+df["is_knockout_stage"] = df["knockout_stage"] == 1
 df = df[["home_team_name", "away_team_name", "match_date", "home_team_score", 
          "away_team_score", "total_goals", "is_knockout_stage"]]
+
+print(df.head())
