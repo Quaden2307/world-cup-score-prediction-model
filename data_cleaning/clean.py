ccb59a45487df060e3464d5db3217b00d1867ced
Caden <cadensun07@gmail.com>
2026-06-09 21:38:53 -0700

    average goals feature

--- data_cleaning/clean.py
+++ data_cleaning/clean.py
@@ -7,4 +7,10 @@ df["is_knockout_stage"] = df["knockout_stage"] == 1
 df = df[["home_team_name", "away_team_name", "match_date", "home_team_score", 
          "away_team_score", "total_goals", "is_knockout_stage"]]
 
-print(df.head())
+home_df = pd.DataFrame({"country": df["home_team_name"], "score": df["home_team_score"]})
+away_df = pd.DataFrame({"country": df["away_team_name"], "score": df["away_team_score"]})
+
+team_avg = pd.concat([away_df, home_df])
+team_avg = team_avg.groupby("country")["score"].mean()
+
+print(team_avg.tail(10))
